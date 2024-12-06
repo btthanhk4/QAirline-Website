6846bee5a3a63e4203e8b02efe73bef350455e4c
tungthanh11 <tungthoihn2004@gmail.com>
2024-12-06 11:48:09 +0700

    add ticketBookingForm and assets

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import FlightSearchForm from './components/FlightSearchForm';
+import SearchResults from './components/SearchResults';
+import BookTicketBox from './TicketBookingForm/BookTicketBox';
+import './App.css';
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (data) => {
+    setResults([data]); // Cập nhật kết quả tìm kiếm
+  };
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Chào mừng đến QAirLine!</h1>
+        <p>Ứng dụng đặt vé máy bay của bạn.</p>
+        <FlightSearchForm onSearch={handleSearch} />
+        <SearchResults results={results} />
+      </main>
+      <Footer />
+      <BookTicketBox></BookTicketBox>
+    </div>
+  );
+}
+
+export default App;
